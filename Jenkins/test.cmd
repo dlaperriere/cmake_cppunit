391955544438d95f8e0bb13844cb298a4f885361
David Laperriere <dlaperriere@hotmail.com>
2016-11-02 11:26:33 -0400

    add Debug configuration to windows test script

--- Jenkins/test.cmd
+++ Jenkins/test.cmd
@@ -3,8 +3,12 @@
 echo "Run tests "
 
 cd build
-copy /Y cppunit-src\bin\Release\cppunit.dll Release\
+
+copy /Y cppunit-src\bin\Release\*.dll Release\
 Release\Test_cppunit 2> Test_cppunit.UnitTest.xml
+
+copy /Y cppunit-src\bin\Debug\*.dll Debug\
+Debug\Test_cppunit 2> Test_cppunit.Debug.UnitTest.xml
 
 cd ..
 
