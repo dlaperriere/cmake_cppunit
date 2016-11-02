2eff8ae9c0c483bb03cba8a714bc3ca3f7ed12e2
David Laperriere <dlaperriere@hotmail.com>
2016-11-02 11:25:08 -0400

    add Visual Studio command line setup and Debug configuration to windows build script

--- Jenkins/build.cmd
+++ Jenkins/build.cmd
@@ -2,10 +2,15 @@
 
 echo "Build cppunit "
 
+rem  Visual Studio 2015 Command Line
+call "%VS140COMNTOOLS%"vsvars32.bat
+
+rmdir /S /Q build
 mkdir build
 cd build
 
 cmake ..
-msbuild /p:Configuration=Release UnitTest.sln
+msbuild /p:Configuration=Release UnitTest.sln /v:quiet
+msbuild /p:Configuration=Debug UnitTest.sln /v:quiet
 
 cd ..
