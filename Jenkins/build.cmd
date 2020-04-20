2bcd9df07c23322a25d3ead123403f822195638c
David Laperriere <dlaperriere@hotmail.com>
2020-04-20 19:14:46 -0400

    update Jenkins build script to use config_vstudio.cmd

--- Jenkins/build.cmd
+++ Jenkins/build.cmd
@@ -1,16 +1,27 @@
 @echo off
 
 echo "Build cppunit "
+echo "---------------------------------------------------"
 
-rem  Visual Studio 2015 Command Line
-call "%VS140COMNTOOLS%"vsvars32.bat
+rem Config Visual Studio Command Line
+call "%~dp0%"config_vstudio.cmd
+
+rem Run cmake & msbuild
+if %vs_ok%==1 (
 
 rmdir /S /Q build
 mkdir build
 cd build
 
+echo "---------------------------------------------------"
+cmake --version
 cmake ..
+
+echo "---------------------------------------------------"
 msbuild /p:Configuration=Release UnitTest.sln /v:quiet
 msbuild /p:Configuration=Debug UnitTest.sln /v:quiet
 
 cd ..
+)
+
+rem -30-
